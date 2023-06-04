9b281d37457149f01481df43dce3fa5c6207ae5e
Denis <71621555+mrklv01@users.noreply.github.com>
2023-06-05 01:49:51 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "localhost",
-      "localhost/",
-      "localhost/api/",
-      "localhost/api/files/",
-      "http://localhost",
-      "http://localhost:8080/api/files/",
-      "http://localhost:8080/api/",
-      "http://localhost/",
-      "https://gruvibed-back.herokuapp.com/",
-      "https://gruvibed-back.herokuapp.com/api",
-      "https://gruvibed-back.herokuapp.com/api/files",
-      "https://gruvibed-back.herokuapp.com/api/files/uploads",
-      "gruvibed-back.herokuapp.com/",
-      "gruvibed-back.herokuapp.com/api",
-      "gruvibed-back.herokuapp.com/api/files",
-      "gruvibed-back.herokuapp.com/api/files/uploads"
-      
-    ],
-  },
+ images: {
+      domains: [
+"https://gruvibed-back.herokuapp.com/",
+"https://gruvibed-back.herokuapp.com/api",
+"gruvibed-back.herokuapp.com/",
+"gruvibed-back.herokuapp.com/api"
+      ],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "gruvibed-back.herokuapp.com/api",
+        },
+      ],
+    },
 };
 
 module.exports = nextConfig;
