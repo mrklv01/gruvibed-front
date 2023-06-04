ee94c16087054dca858359b8dd32ac64c5d1fdcc
Denis <71621555+mrklv01@users.noreply.github.com>
2023-06-05 01:55:43 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
- images: {
+const nextjsDistDir = join("src", require("./src/next.config.js").distDir);
+const nextjsServer = next({
+  dev: isDev,
+  conf: {
+    distDir: nextjsDistDir,
+    images: {
       domains: [
 "https://gruvibed-back.herokuapp.com/",
 "https://gruvibed-back.herokuapp.com/api",
 "gruvibed-back.herokuapp.com/",
 "gruvibed-back.herokuapp.com/api"
       ],
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "gruvibed-back.herokuapp.com/api",
-        },
-      ],
-    },
-};
+    }
+  }
+});
 
-module.exports = nextConfig;
+module.exports = nextjsServer;
